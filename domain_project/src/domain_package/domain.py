2f02f436e28b9ab5dd33908fc5c44236cf875228
Philipp.DAngelo.external <philipp_angelo@gmx.de>
2022-09-01 22:08:51 +0200

    create initial domain tests

--- domain_project/src/domain_package/domain.py
+++ domain_project/src/domain_package/domain.py
@@ -1,7 +1,21 @@
-class SKU:
+from dataclasses import dataclass
+from tkinter.ttk import Style
 
-    def __init__(self) -> None:
-        pass
+@dataclass
+class OrderLine:
+    order_id: str
+    product: str
+    qty: int
 
-    def hello(self):
-        print('hello')
+class Batch:
+
+    def __init__(self, reference_id, sku, qty=0, eta='stocked' ) -> None:
+        self.reference_id = reference_id
+        self.sku = sku
+        self.available_qty = qty
+        self.eta = eta
+    
+    def allocate(self, line):
+        self.available_qty -= line.qty
+    
+
--- test/test_domain.py
+++ test/test_domain.py
@@ -1,13 +1,29 @@
 from difflib import unified_diff
 import unittest
-import domain_project
+from domain_package import OrderLine, Batch
+import datetime as dt
 
 class DomainModelTest(unittest.TestCase):
 
-    def test_sku(self):
-        self.assertEqual(1,1)
+    def test_successfull_allocation(self):
+        batch = Batch('batch-001', 'SMALL-Table', qty=20, eta=dt.datetime.today())
+        line = OrderLine('order-001', 'SMALL-Table', 2)
+
+        batch.allocate(line)
+    
+        self.assertEqual(batch.available_qty,18)
+
+    def test_failed_allocation(self):
+        batch = Batch('batch-001', 'SMALL-Table', qty=1, eta=dt.datetime.today())
+        line = OrderLine('order-001', 'SMALL-Table', 2)
+
+        batch.allocate(line)
+        self.assertRaises()
+        self.assertEqual(batch.available_qty,18)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()    
-    
-    domain_package.sku.hello()
+
