9372304e5596ba1a46df5a7d187c7f1ec7ea9df0
Philipp.DAngelo.external <philipp_angelo@gmx.de>
2022-09-03 11:22:05 +0200

    add allocation methods and checks

--- domain_project/src/domain_package/domain.py
+++ domain_project/src/domain_package/domain.py
@@ -1,21 +1,51 @@
 from dataclasses import dataclass
 from tkinter.ttk import Style
 
-@dataclass
+@dataclass(frozen=True)
 class OrderLine:
     order_id: str
-    product: str
+    sku: str
     qty: int
 
 class Batch:
 
     def __init__(self, reference_id, sku, qty=0, eta='stocked' ) -> None:
-        self.reference_id = reference_id
+        self._reference_id = reference_id
         self.sku = sku
-        self.available_qty = qty
         self.eta = eta
+        self._allocations = set()
+        self._purchased_qty = qty
     
-    def allocate(self, line):
-        self.available_qty -= line.qty
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def can_allocate(self, line: OrderLine):
+        return self.available_qty >= line.qty and self.sku == line.sku
+    
+    def deallocate(self, line):
+        if line in self._allocations:
+            self.allocated_orders.remove(line.order_id)
+
+    @property
+    def allocated_qty(self):
+        return sum([line.qty for line in self._allocations])
+
+    @property
+    def available_qty(self):
+        return self._purchased_qty - self.allocated_qty
+
+    @property
+    def reference_id(self):
+        return self._reference_id
+
+    def __eq__(self, other):
+        if not isinstance(other, Batch):
+            return False
+        else:
+            return other.reference_id == self.reference_id
+    
+    def __hash__(self):
+        return hash(self.reference_id)
     
 
--- test/test_domain.py
+++ test/test_domain.py
@@ -1,25 +1,50 @@
-from difflib import unified_diff
+
 import unittest
 from domain_package import OrderLine, Batch
 import datetime as dt
 
 class DomainModelTest(unittest.TestCase):
 
+    def create_batch_and_order(self, batch_id, batch_qty, product, order_id, order_qty):
+        batch = Batch(batch_id, product, qty=batch_qty, eta= dt.datetime.today())
+        line = OrderLine(order_id, product, order_qty)
+        return batch, line
+
     def test_successfull_allocation(self):
-        batch = Batch('batch-001', 'SMALL-Table', qty=20, eta=dt.datetime.today())
-        line = OrderLine('order-001', 'SMALL-Table', 2)
-
+        batch, line = self.create_batch_and_order('batch-001', 20, 'SMALL-Table', 'order-001', 2)
         batch.allocate(line)
+        self.assertEqual(batch.available_qty,18)
+
+    def test_can_be_allocated(self):
+        batch, line = self.create_batch_and_order('batch-001', 20, 'SMALL-Table', 'order-001', 2)
+        self.assertTrue(batch.can_allocate(line))
     
-        self.assertEqual(batch.available_qty,18)
+    def test_cannot_be_allocated_not_enough_qty(self):
+        batch, line = self.create_batch_and_order('batch-001', 2, 'SMALL-Table', 'order-001', 4)
+        self.assertFalse(batch.can_allocate(line))
 
-    def test_failed_allocation(self):
-        batch = Batch('batch-001', 'SMALL-Table', qty=1, eta=dt.datetime.today())
-        line = OrderLine('order-001', 'SMALL-Table', 2)
+    def test_cannot_be_allocated_product_mismatch(self):
+        batch = Batch('batch-001', 'SMALL-Table', qty=20, eta= dt.datetime.today())
+        line = OrderLine('order-001', 'LARGE-Table', 2)
+        self.assertFalse(batch.can_allocate(line))
 
+    def test_deallocate_only_allocated_line(self):
+        batch, line = self.create_batch_and_order('batch-001', 20, 'SMALL-Table', 'order-001', 2)
+        batch.deallocate(line)
+        self.assertEqual(batch.available_qty, 20)
+
+    def test_ensure_no_double_handline(self):
+        batch, line = self.create_batch_and_order('batch-001', 20, 'SMALL-Table', 'order-001', 2)
         batch.allocate(line)
-        self.assertRaises()
-        self.assertEqual(batch.available_qty,18)
+        batch.allocate(line)
+        self.assertEqual(batch.available_qty, 18)
+
+    def test_reference_id_is_readonly(self):
+        batch = Batch('batch-001', 'SMALL-Table', qty=20, eta= dt.datetime.today())
+        with self.assertRaises(AttributeError) as context:
+            batch.reference_id = 'new_id'
+
+
 
 
 
